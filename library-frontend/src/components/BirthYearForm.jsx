645be87a8aad253d7c7f43db336d6c64398ef5b1
Artem <artemm3note@gmail.com>
2024-08-08 00:15:17 +0300

    Updated the birthyear form. Now a name input has a handy selection among existed authors.

--- library-frontend/src/components/BirthYearForm.jsx
+++ library-frontend/src/components/BirthYearForm.jsx
@@ -1,9 +1,11 @@
 import { useMutation } from '@apollo/client'
 import { useField } from '../hooks/useField'
 import { ALL_AUTHORS, EDIT_AUTHOR_BIRTH } from '../queries'
+import Select from 'react-select'
+import { useState } from 'react'
 
-const BirthYearForm = () => {
-    const [name, resetName] = useField('text')
+const BirthYearForm = ({ authors }) => {
+    const [name, setName] = useState(null)
     const [born, resetBorn] = useField('number')
 
     const [ EditAuthorBirth ] = useMutation(EDIT_AUTHOR_BIRTH, {
@@ -20,18 +22,22 @@ const BirthYearForm = () => {
             }
         })
 
-        resetName()
         resetBorn()
     }
+
+    const options = authors.map(author => {
+        return { value: author.name, label: author.name }
+    })
     
     return (
         <div>
             <h2>Set birthyear</h2>
             <form onSubmit={updateBirthYear}>
-                <div>
-                    name
-                    <input {...name} />
-                </div>
+                <Select 
+                    defaultValue={name}
+                    onChange={setName}
+                    options={options}
+                />
                 <div>
                     born
                     <input {...born} />
